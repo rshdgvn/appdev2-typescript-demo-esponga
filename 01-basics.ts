af18a8d7b538c1b9c6e2c56e29052bfa038d081d
rshdgvn <rasheedgavinesponga@gmail.com>
2026-03-26 02:35:41 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,20 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Gavin";
+let userAge: string | number = 20;
 
 // ...
 
-userAge = "31"
+userAge = "20"
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b: number): number {
     return a + b
 }
 
-add(10)
+add(10, 12)
 // add('10')
-add(10, 6)
+add(10, 10)
 // add(10, '6')
