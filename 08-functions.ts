3505cf066ac7134ddba210f90f38cd87952f2152
rshdgvn <rasheedgavinesponga@gmail.com>
2026-05-08 22:18:45 +0800

    Done playing and updating the functions.ts

--- 08-functions.ts
+++ 08-functions.ts
@@ -2,40 +2,53 @@
 
     // Function Return Value Types
     function add(a: number, b: number): number { // you can omit the function type since typescript can infers it
-        return a + b
+        const sum =  a + b
+
+        return sum.toString() // type error only number can be returned
     }
 
-    add(5, 10)
+    add(6, 7)
     // add("5", 10)
 
     // Special Type `void` - return nothing
     // Function doesn't return a meaningful value, but it does finish
-    function log(message: string): void {
+    function log(message: string | number): void {
         console.log(message)
+
     }
 
-    log("Hello, World!")
+    log(67)
 
     // Another Special Type `never`
     // Function never finishes normally (throws error, infinite loop)
-    function logAndThrow(errorMessage: string) {
+    function logAndThrow(errorMessage: string): never {
         console.log(errorMessage)
+        return "Try" // function error
         throw new Error(errorMessage)
     }
+
+    logAndThrow("This is an error")
 
     // Function as Types
     // Useful for callback functions
     // () => {} is not same as () => void
     // () => {} - arrow function
     // () => void - function type
-    function performJob(cb: (m: string) => void) {  // you can also use cb: Function but () => void is the best practice
-        // ...
-        cb('Job Done!')
+    function performJob(canbeany: (m: string) => void) {  // you can also use cb: Function but () => void is the best practice
+        canbeany('Job Done!')
     }
+
+    function notBestPractice(cb: Function) {
+        cb('This is not best practice')
+    }
+
 
     let logMsg = (msg: string): void => {
         console.log(msg)
     }
+
+
+    logMsg('Hello from callback function')
 
     performJob(logMsg)
 
@@ -44,14 +57,12 @@
         name: string;
         age: number;
         greet: () => string;
+
     }
 
-    let user: User = {
-        name: "Elmer",
-        age: 39,
-        // greet: function() {
-        //     return this.name
-        // }
+    const user: User = {
+        name: "rshdgvn",
+        age: 20,
         greet() {
             console.log(`Hello, ${this.name}`)
             return this.name
